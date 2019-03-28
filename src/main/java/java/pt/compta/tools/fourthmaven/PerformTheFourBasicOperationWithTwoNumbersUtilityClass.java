b10c37e406a7feeaeb2aeb6305edb2bbe23fd714
Icefyred <dcastilho001@hotmail.com>
2019-03-28 19:50:23 +0000

    Added two new methods to the utility class

--- src/main/java/java/pt/compta/tools/fourthmaven/PerformTheFourBasicOperationWithTwoNumbersUtilityClass.java
+++ src/main/java/java/pt/compta/tools/fourthmaven/PerformTheFourBasicOperationWithTwoNumbersUtilityClass.java
@@ -1,12 +1,21 @@
 package java.pt.compta.tools.fourthmaven;
 
-public abstract class AddOrSubtractTwoNumbersUtilityClass {
+public abstract class PerformTheFourBasicOperationWithTwoNumbersUtilityClass {
 	
 	public static int sumOfTwoNumbers(int firstNumber, int secondNumber) {
 		return firstNumber + secondNumber;
 	}
+	
 	public static int subtractOfTwoNumbers(int firstNumber, int secondNumber) {
 		return firstNumber - secondNumber;
+	}
+	
+	public static int multiplyOfTwoNumbers(int firstNumber, int secondNumber) {
+		return firstNumber * secondNumber;
+	}
+	
+	public static int divideOfTwoNumbers(int firstNumber, int secondNumber) {
+		return firstNumber / secondNumber;
 	}
 }
 	/*
