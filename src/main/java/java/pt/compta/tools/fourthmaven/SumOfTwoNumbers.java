3bebdfce3ba8d64e0bc88cb46b5ebb9764656243
Icefyred <dcastilho001@hotmail.com>
2019-03-28 18:10:27 +0000

    Changed the utility class so it can never be instantiated

--- src/main/java/java/pt/compta/tools/fourthmaven/SumOfTwoNumbers.java
+++ src/main/java/java/pt/compta/tools/fourthmaven/SumOfTwoNumbers.java
@@ -1,6 +1,6 @@
 package java.pt.compta.tools.fourthmaven;
 
-public class SumOfTwoNumbers {
+public abstract class SumOfTwoNumbers {
 	
 	public static int sumOfTwoNumbers(int firstNumber, int secondNumber) {
 		return firstNumber + secondNumber;
