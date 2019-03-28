7f6e0c56b923f7d9454a6d1c36047f5260a94622
Icefyred <dcastilho001@hotmail.com>
2019-03-28 19:43:19 +0000

    Changing class name for test in order to have it properly named in GitHub

--- src/main/java/java/pt/compta/tools/fourthmaven/TestPhase.java
+++ src/main/java/java/pt/compta/tools/fourthmaven/TestPhase.java
@@ -1,6 +1,6 @@
 package java.pt.compta.tools.fourthmaven;
 
-public abstract class AddOrSubtractTwoNumbers {
+public abstract class TestPhase {
 	
 	public static int sumOfTwoNumbers(int firstNumber, int secondNumber) {
 		return firstNumber + secondNumber;
